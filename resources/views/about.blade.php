cecfeba860ce3c96348266e87cb53363178977b3
brokoline <125354647+brokoline@users.noreply.github.com>
2024-10-08 13:29:01 +0200

    Dag 4 - Headings på sider

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+<x-slot:heading>
+        About page
+    </x-slot:heading>
     <h1>Hej fra om-siden</h1>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Home page
+    </x-slot:heading>
     <h1>Hej fra hjemmesiden</h1>
 </x-layout>
 
