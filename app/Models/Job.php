aa21224b0d26cb03dd0aeab595fab988db78e56f
brokoline <125354647+brokoline@users.noreply.github.com>
2024-10-16 10:10:31 +0200

    Virker ikke heeeelt....... som i videoen....

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -19,5 +19,12 @@ class Job extends Model {
         return $this->belongsTo(Employer::class);
     }
 
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey:"job_listing_id");
+    }
+
+
 }
 
