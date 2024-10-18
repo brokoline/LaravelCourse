9a93cce708970d882a071a3a24ae4badc16f62c6
brokoline <125354647+brokoline@users.noreply.github.com>
2024-10-18 11:40:48 +0200

    Eager loading and the N+1 Problem and debug bar

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -22,7 +22,7 @@ class Job extends Model {
 
     public function tags()
     {
-        return $this->belongsToMany(Tag::class, foreignPivotKey:"job_listing_id");
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
     }
 
 
